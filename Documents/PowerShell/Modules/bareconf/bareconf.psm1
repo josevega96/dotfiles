95bafe1e6b5c42adcd1180f82a83fec7d80c9bfb
josevega96 <jdelavega36@gmail.com>
2021-03-25 12:55:37 -0600

    Addd more args to bareconf

--- Documents/PowerShell/Modules/bareconf/bareconf.psm1
+++ Documents/PowerShell/Modules/bareconf/bareconf.psm1
@@ -3,7 +3,8 @@ function bareconf {
 	 	$arg1,
 		$arg2,
 		$arg3,
-		$arg4
+		$arg4,
+		$arg5
 	)
-	git --git-dir=$env:USERPROFILE/.cfg/ --work-tree=$env:USERPROFILE $arg1 $arg2 $arg3 $arg4 
+	git --git-dir=$env:USERPROFILE/.cfg/ --work-tree=$env:USERPROFILE $arg1 $arg2 $arg3 $arg4 $arg5 
 }
