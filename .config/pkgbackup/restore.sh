9754dda1e314640ba0c4d51d2c56e243cc4b07a3
josevega96 <jdelavega36@gmail.com>
2021-12-10 17:20:55 -0600

    Add packages backup to debian install

---
+++ .config/pkgbackup/restore.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+##
+#script for resotoring debian packages
+##
+
+sudo apt-key add ~/Repo.keys
+sudo cp -R ~/sources.list* /etc/apt/
+sudo apt-get update
+sudo apt-get install dselect
+sudo dselect update
+sudo dpkg --set-selections < ~/Package.list
+sudo apt-get dselect-upgrade -y
